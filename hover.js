0527291e01d0c1b574fb87af855b260dc3fb768a
Rafal Liszewski <raffa.lisz@gmail.com>
2020-04-13 13:34:08 +0930

    Hover working bitch.
    Added javascript dependcy for hover.js.
    Added more images into image/ folder.
    New code for index & style.
    Only edits for hover, nothing else added.

---
+++ hover.js
@@ -0,0 +1,8 @@
+// the ui-resizable-handles are added here
+$('.resizable').resizable();
+
+// makes GSAP Draggable avoid clicks on the resize handles
+$('.ui-resizable-handle').attr('data-clickable', true);
+
+// make the element draggable
+Draggable.create('.draggable');
